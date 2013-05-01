ca8dd37c2af4cd778ed879c1d05b79a0d645709d
Vaibhav Gupta <kacxdak@gmail.com>
2013-05-01 06:40:45 -0500

    Added quick analysis and some fixed graphs

--- game/agent.cpp
+++ game/agent.cpp
@@ -19,9 +19,9 @@ Agent::Agent( Game* gmptr) {
 	for(int i = 0; i < 10; i++)
 	for(int j = 0; j < 10; j++)
 	for(int k = 0; k < 2; k++) {
-		qval[i][j][k][STAY] = 1;
+		qval[i][j][k][STAY] = 2;
 		visit[i][j][k][STAY] = 0;
-		qval[i][j][k][HIT] = 1;
+		qval[i][j][k][HIT] = 2;
 		visit[i][j][k][HIT] = 0;
 	}
 
